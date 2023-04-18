cd2e5875043eeb47750b975e6b0658ffb6e129a5
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-18 12:14:08 -0300

    Modelo persona

---
+++ webApp/src/main/java/com/sistemasactivos/webapp/model/Persona.java
@@ -0,0 +1,25 @@
+package com.sistemasactivos.webapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Persona {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private String ciudad;
+    private String pais;
+    private String codigoPostal;
+    private String cuit;
+}
